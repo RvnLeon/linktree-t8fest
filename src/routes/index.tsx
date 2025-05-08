a4ff16749ecf0aa13e718777409bbd3454f45b78
RvnLeon <rezasfel720@gmail.com>
2025-05-08 14:11:44 +0700

    Implement code changes to enhance functionality and improve performance

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,7 @@
 import { createFileRoute } from '@tanstack/react-router'
-import logoImage from '../assets/logo.png'
+import logoImage from '../assets/photos/logo.png'
 import { LinkButton } from '../components/LinkButton'
-import { Star, BookOpenText, PhoneCallIcon } from 'lucide-react'
+import { BookOpenText, Contact } from 'lucide-react'
 
 export const Route = createFileRoute('/')({
   component: App,
@@ -9,16 +9,43 @@ export const Route = createFileRoute('/')({
 
 function App() {
   return (
-    <div className="h-screen w-full flex flex-col items-center justify-center bg-[#1b5ee1] p-4">
-      <div className="flex flex-col items-center h-full w-full max-w-lg bg-[#f3f3f3] rounded-md p-4 overflow-y-auto gap-4">
-        <img src={logoImage} alt="logo.png" className="size-32 mb-4" />
-        <h1 className="mb-6 text-4xl font-bold">T8CHNOFEST</h1>
+    <div className="h-screen w-full flex flex-col items-center justify-center bg-[url(/assets/photos/bg.jpeg)] p-4">
+      <div className="flex flex-col items-center h-full w-full max-w-lg bg-[#f3f3f3] shadow-xl rounded-md px-4 py-8 overflow-y-auto gap-4">
+        <div className="size-32 p-4 bg-zinc-200 rounded-full">
+          <img src={logoImage} alt="logo.png" className="size-full" />
+        </div>
+        <h1 className="text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-tx-400 to-tx-500 drop-shadow-[0_4px_4px_rgba(0,0,0,0.4)]">
+          T8CHNOFEST
+        </h1>
+        <p className="-mt-4 text-sm">Cerdas, Tangguh, Peduli</p>
 
         <div className="flex flex-col gap-4 size-full">
-          <LinkButton icon={BookOpenText} label="Juklak T8" to="" />
-          <LinkButton icon={BookOpenText} label="Juknis T8" to="" />
-          <LinkButton icon={Star} label="Test" to="" />
-          <LinkButton icon={PhoneCallIcon} label="Contact Person" to="" />
+          <LinkButton icon={BookOpenText} label="Juklak Juknis T8" to="" />
+          <LinkButton
+            icon={BookOpenText}
+            label="CP Umum"
+            to="https://wa.me/6287882829131"
+          />
+          <LinkButton
+            icon={Contact}
+            label="CP Turnamen"
+            to="https://wa.me/6287764825736"
+          />
+          <LinkButton
+            icon={Contact}
+            label="CP Mobile Legend"
+            to="https://wa.me/6289531671507"
+          />
+          <LinkButton
+            icon={Contact}
+            label="CP Cerdas Cermat Informatika"
+            to="https://wa.me/6285215319829"
+          />
+          <LinkButton
+            icon={Contact}
+            label="CP Web Design"
+            to="https://wa.me/6281905318471"
+          />
         </div>
       </div>
     </div>
