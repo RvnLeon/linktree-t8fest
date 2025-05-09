b5ab261cad7ec5b189ea55e21bda3b75f466dc43
RvnLeon <rezasfel720@gmail.com>
2025-05-09 21:17:34 +0700

    feat: add radix-ui for tab navigation in juknis route and update package.json

--- src/routes/juknis.lazy.tsx
+++ src/routes/juknis.lazy.tsx
@@ -1,4 +1,6 @@
 import { createLazyFileRoute } from '@tanstack/react-router'
+import { useState } from 'react'
+import { Tabs } from 'radix-ui'
 
 export const Route = createLazyFileRoute('/juknis')({
   component: RouteComponent,
@@ -8,7 +10,24 @@ function RouteComponent() {
   return (
     <div className="relative h-screen w-full flex items-center justify-center p-4 overflow-hidden">
       <div className="absolute inset-0 bg-[url(/assets/photos/bg.jpeg)] bg-cover filter blur-[2px] z-0"></div>
-      <div className="h-full w-full max-w-lg bg-gradient-to-b from-white to-zinc-100 shadow-xl rounded-md overflow-hidden z-10 p-4"></div>
+      <div className="h-full w-full max-w-2xl bg-gradient-to-b from-white to-zinc-100 shadow-xl rounded-md overflow-hidden z-10 p-4">
+        <div className="border-b border-zinc-300/50 pb-4 mb-4">
+          <Tabs.List className="max-w-full bg-blue-500 p-4 rounded-md flex flex-row items-center">
+            <Tabs.Trigger
+              className="bg-zinc-400 rounded-xl p-2 flex justify-center items-center"
+              value="tab1"
+            >
+              Ketentuan Umum
+            </Tabs.Trigger>
+            <Tabs.Trigger
+              className="bg-zinc-400 rounded-xl p-2 flex justify-center items-center"
+              value="tab1"
+            >
+              Ketentuan Khusus
+            </Tabs.Trigger>
+          </Tabs.List>
+        </div>
+      </div>
     </div>
   )
 }
